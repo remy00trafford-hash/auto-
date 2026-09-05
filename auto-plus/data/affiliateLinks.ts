fd221f05684b1f49c62c867e6c64162be76d4031
remy trafford <remy00trafford@gmail.com>
2026-09-05 14:43:25 +0200

    feat: integrate 5 new AliExpress affiliate products

--- auto-plus/data/affiliateLinks.ts
+++ auto-plus/data/affiliateLinks.ts
@@ -2,4 +2,9 @@ export const affiliateLinks: Record<string, string> = {
   'support-telephone-gravite-ventilation': 'https://s.click.aliexpress.com/e/_c3OV9JnP',
   'essager-chargeur-voiture-54w': 'https://s.click.aliexpress.com/e/_c3Ypx32V',
   'joyroom-support-telephone-360': 'https://s.click.aliexpress.com/e/_c4VsBiwD',
+  'support-telephone-universel-long-bras': 'https://s.click.aliexpress.com/e/_c3JARhch',
+  'led-ambiance-rgb-voiture': 'https://s.click.aliexpress.com/e/_c3esb3Jf',
+  'housse-volant-seametal-38cm': 'https://s.click.aliexpress.com/e/_c3cweypJ',
+  'organisateur-siege-auto': 'https://s.click.aliexpress.com/e/_c32iCHW9',
+  'kit-brosses-detailing-auto': 'https://s.click.aliexpress.com/e/_c3xjQNYp',
 };
